396cc06b0b9f1554d3bc62e9165752c05b343d5f
Slindex <david.echajaya@hotmail.com>
2025-10-03 17:38:20 -0500

    rotation feature added

--- main.py
+++ main.py
@@ -20,9 +20,10 @@ def main():
                 return
         screen.fill("black")
         player.draw(screen)
+        dt = clock.tick(60) / 1000
+        player.update(dt)
         pygame.display.flip()
         clock.tick(60)
-        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
