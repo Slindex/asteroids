6c36e6f884879929bdffa0a39ddd7a133cd4c25d
Slindex <david.echajaya@hotmail.com>
2025-10-03 18:10:26 -0500

    groups added

--- main.py
+++ main.py
@@ -11,18 +11,28 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        player.draw(screen)
+            
         dt = clock.tick(60) / 1000
-        player.update(dt)
+        
+        screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
+
         clock.tick(60)
 
 
